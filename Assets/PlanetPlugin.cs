3072149f150f32d37432c0261dc25bc2bda00c3f
B-10515025 <ddt22771508@gmail.com>
2017-02-12 07:57:40 +0800

    4+

---
+++ Assets/PlanetPlugin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PlanetPlugin : Editor {
+    [MenuItem("PlanetPlugin/CreatePlanet")]
+    static void CreatePlanet()
+    {
+        GameObject.Find("Directional Light").GetComponent<Light>().intensity = 0;
+        GameObject highlighter = new GameObject("highlighter");
+        highlighter.AddComponent<Highlighter>();
+        GameObject planet = new GameObject("planet");
+        planet.AddComponent<Planet>();
+        MeshRenderer mr = planet.AddComponent<MeshRenderer>();
+        MeshFilter filter = planet.AddComponent<MeshFilter>();
+        mr.material = new Material(Shader.Find("Specular"));
+    }
+}
